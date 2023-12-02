55021b2e453fd272a5e09284c2117dcbe71f8bf0
Rake <test@example.org>
2023-12-02 21:50:41 +0300

    fix: comman

--- 2-dice.js
+++ 2-dice.js
@@ -3,10 +3,10 @@ function dice(d) {
     let res = d.slice(1)
     return Math.floor((Math.random() * Number(res)) + 1);
 }
-console.log(dice('d4'))
-console.log(dice('d6'))
-console.log(dice('d8'))
-console.log(dice('d10'))
-console.log(dice('d12'))
-console.log(dice('d16'))
-console.log(dice('d20'))
+console.log(dice('d4'));
+console.log(dice('d6'));
+console.log(dice('d8'));
+console.log(dice('d10'));
+console.log(dice('d12'));
+console.log(dice('d16'));
+console.log(dice('d20'));
