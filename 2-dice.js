a7dd7c51888ae6e7d095819f62076f4c68475c02
Rake <test@example.org>
2023-12-02 21:43:18 +0300

    random dice

---
+++ 2-dice.js
@@ -0,0 +1,12 @@
+"use strict";
+function dice(d) {
+    let res = d.slice(1)
+    return Math.floor((Math.random() * Number(res)) + 1);
+}
+console.log(dice('d4'))
+console.log(dice('d6'))
+console.log(dice('d8'))
+console.log(dice('d10'))
+console.log(dice('d12'))
+console.log(dice('d16'))
+console.log(dice('d20'))
